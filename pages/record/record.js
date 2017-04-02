5369718751fa2ba2fa490de41b8a84b6681649d4
je_ffy <jzxiaobao@gmail.com>
2017-04-02 22:19:46 +0800

    使用picker

--- pages/record/record.js
+++ pages/record/record.js
@@ -4,11 +4,13 @@ var app = getApp()
 Page({
   data:{
     descLabel: '小姨妈来了',
-    userInfo:{}
+    userInfo:{},
+    date: '2016-09-01',
+
   },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
-    var that = this
+    var that = this;
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function(userInfo){
       //更新数据
@@ -18,21 +20,21 @@ Page({
       })
 
     })
-  //   ,
-  //    toDetail:function(){
-  //   wx.navigateTo({
-  //     url: '/pages/detail/detail',
-  //     success: function(res){
-  //       // success
-  //     },
-  //     fail: function() {
-  //       // fail
-  //     },
-  //     complete: function() {
-  //       // complete
-  //     }
-  //   })
-  // }
+  },
+  //first method
+  toDetail:function(){
+    
+
+  },
+  // datapicker
+  bindDateChange: function(e) {
+    console.log('onLoad99999999999')
+    this.setData({
+      date: e.detail.value
+    })
+
+
+      console.log( e.detail.value)
   },
   onReady:function(){
     // 页面渲染完成
