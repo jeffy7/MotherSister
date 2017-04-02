c7be9188589db3f378a778661b4a5de93c5cd619
je_ffy <jzxiaobao@gmail.com>
2017-04-02 13:49:44 +0800

    no message

--- pages/record/record.js
+++ pages/record/record.js
@@ -1,8 +1,38 @@
 // pages/record/record.js
+var app = getApp()
+
 Page({
-  data:{},
+  data:{
+    descLabel: '小姨妈来了',
+    userInfo:{}
+  },
   onLoad:function(options){
     // 页面初始化 options为页面跳转所带来的参数
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      console.log(userInfo)
+      that.setData({
+        userInfo:userInfo
+      })
+
+    })
+  //   ,
+  //    toDetail:function(){
+  //   wx.navigateTo({
+  //     url: '/pages/detail/detail',
+  //     success: function(res){
+  //       // success
+  //     },
+  //     fail: function() {
+  //       // fail
+  //     },
+  //     complete: function() {
+  //       // complete
+  //     }
+  //   })
+  // }
   },
   onReady:function(){
     // 页面渲染完成
