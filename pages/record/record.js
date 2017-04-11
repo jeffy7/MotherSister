514e30e4b7877f043a14ddac0c8ac470504e80cc
je_ffy <jzxiaobao@gmail.com>
2017-04-11 22:00:07 +0800

    no message

--- pages/record/record.js
+++ pages/record/record.js
@@ -12,18 +12,29 @@ Page({
   data:{
     descLabel: '小姨妈来了',
     userInfo:{},
-    date: '2017-04-01',
+    dateStartValue:'',
+    dateEndValue:'',
+    dateValue: util.formatNormalTime(new Date()),
     iscome111:0,
   },
 
   onLoad:function(options){
-         console.log( util.formatTime(new Date()))
-
     //是否来小姨妈
     pageData.iscome = app.globalData.userData.iscome
+
+    if(app.globalData.userData.iscome == '1'){
+      //如果来了小姨妈，picker的开始时间应该从记录开始的时间取
+      pageData.dateStartValue = app.globalData.userData.starttime,
+      pageData.dateEndValue = '2018-01-01'
+    }else {
+      //没有来小姨妈，开始时间与结束时间随便设置
+      pageData.dateStartValue = '2015-01-01',
+      pageData.dateEndValue = '2018-01-01'
+    }
     //来了多少天
     pageData.days = util.forComeDays(new Date())
     //预计还有多少天来
+    pageData.willDays = util.forComeDays(new Date())
 
     this.setData(pageData)
    
@@ -42,23 +53,27 @@ Page({
   // datapicker
   bindDateChange: function(e) {
     this.setData({
-      date: e.detail.value
+      dateValue: e.detail.value
     })
-    console.log( e.detail.value)
-    var stringTime = e.detail.value;
-var timestamp2 = Date.parse(new Date(stringTime));
-//2014-07-10 10:21:12的时间戳为：1404958872 
-console.log(stringTime + "的时间戳为：" + timestamp2);
-var date =  new Date(timestamp2)
-var year = date.getFullYear()
-  var month = date.getMonth() + 1
-  var day = date.getDate()
-console.log("yyyy" + year);
-console.log("mm" + month);
-console.log("dd" + day);
+//     console.log( e.detail.value)
+//     var stringTime = e.detail.value;
+// var timestamp2 = Date.parse(new Date(stringTime));
+// //2014-07-10 10:21:12的时间戳为：1404958872 
+// console.log(stringTime + "的时间戳为：" + timestamp2);
+// var date =  new Date(timestamp2)
+// var year = date.getFullYear()
+//   var month = date.getMonth() + 1
+//   var day = date.getDate()
+// console.log("yyyy" + year);
+// console.log("mm" + month);
+// console.log("dd" + day);
 
     var userData = wx.getStorageSync('userData')
     if(pageData.iscome){
+      //设置结束时间
+      this.setData({
+        dateStartValue: e.detail.value
+      })
       pageData.iscome = 0
       //记录一条数据 并且记住结束状态
       userData.iscome = 0
